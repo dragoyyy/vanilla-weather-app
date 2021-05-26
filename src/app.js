948f9442e656b0a3b69f08c1f1f4ce46e6ce9611
dragoyyy <liza.dragoy@gmail.com>
2021-05-26 15:35:05 +0300

    Updated the time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,27 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+    hours = `0${hours}`;
+  }
+    let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+     let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+
+}
+
 function displayTemperature(response) {
     console.log(response.data);
     document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
@@ -5,6 +29,7 @@ function displayTemperature(response) {
     document.querySelector("#description").innerHTML = response.data.weather[0].description;
     document.querySelector("#humidity").innerHTML = response.data.main.humidity;
     document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
+    document.querySelector("#date").innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "473a5a77714b708fee4cf35264cac21d";
