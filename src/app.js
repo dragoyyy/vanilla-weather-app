cf01ab594aa9e94ed8270df0cc987b1403800164
dragoyyy <liza.dragoy@gmail.com>
2021-05-26 16:35:59 +0300

    Added relevant icon from API

--- src/app.js
+++ src/app.js
@@ -23,16 +23,17 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-   
+    
     document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
     document.querySelector("#city").innerHTML = response.data.name;
     document.querySelector("#description").innerHTML = response.data.weather[0].description;
     document.querySelector("#humidity").innerHTML = response.data.main.humidity;
     document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
     document.querySelector("#date").innerHTML = formatDate(response.data.dt * 1000);
+    document.querySelector("#icon").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
-
 let apiKey = "473a5a77714b708fee4cf35264cac21d";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Prague&appid=${apiKey}&units=metric`;
+let city = "Lisbon"
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
